26ad0106f3fb767b82abb415c1de97f056ae3154
M3roof <43769947+M3roof@users.noreply.github.com>
2018-11-18 18:35:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1054,7 +1054,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "اسم الروم");
+    const logChannel = member.guild.channels.find(channel => channel.name === "tèxt");
     logChannel.send(`Invited by: <@${inviter.tag}>`);
   });
 });
