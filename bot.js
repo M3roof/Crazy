bfa1afca84f6a1c7129ad3a84a482ff68024bad6
M3roof <43769947+M3roof@users.noreply.github.com>
2018-11-18 18:04:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1036,7 +1036,7 @@ client.on('message',async message => {
 const fs = require('fs'); // npm i fs
 const ms = require('ms'); // npm i ms
 const cool = [];
-hero.on('message',async message => {
+client.on('message',async message => {
   if(message.author.bot) return;
   if(message.channel.type === 'dm') return;
 
