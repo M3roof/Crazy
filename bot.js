0786df9dba9f2a71639678396fe89d2bbeb87b28
M3roof <43769947+M3roof@users.noreply.github.com>
2018-11-19 22:47:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -196,7 +196,7 @@ message.author.send(`
 ─════════════ {✯اوامر عامة✯} ════════════─
 ❧ Ctime ➺ الوقت و اليوم و التاريخ
 ❧ Cmb ➺ حـألـة الاعضاء
-❧ Soon ➺ قريبا
+❧ Cuser ➺ بـروفـايـلـك
 ❧ Soon ➺ قريبا
 ❧ Soon ➺ قريبا
 ❧ Soon ➺ قريبا
@@ -362,7 +362,7 @@ var prefix = "C"
 
 client.on('message',async msg => {
   var p = "C";
-  if(msg.content.startsWith(p + "user")) {
+  if(msg.content.startsWith(p + "count")) {
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
   msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
@@ -1086,4 +1086,52 @@ client.on('message', function(message) {
     }
     }
     });
+
+client.on('message', message => {
+          
+
+  if (message.content.startsWith(prefix + "user")) {
+   
+   if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات ❌`);
+
+       message.guild.fetchInvites().then(invs => {
+let member = client.guilds.get(message.guild.id).members.get(message.author.id);
+let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
+let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+var moment = require('moment');
+var args = message.content.split(" ").slice(1);
+let user = message.mentions.users.first();
+var men = message.mentions.users.first();
+var heg;
+if(men) {
+heg = men
+} else {
+heg = message.author
+}
+var mentionned = message.mentions.members.first();
+var h;
+if(mentionned) {
+h = mentionned
+} else {
+h = message.member
+}
+moment.locale('ar-TN');
+var id = new  Discord.RichEmbed()
+
+.setColor("#0a0909")
+.setThumbnail(message.author.avatarURL)
+.addField(': تاريخ دخولك للديسكورد',` \`${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} \`**\n ${moment(heg.createdTimestamp).fromNow()}**` ,true) 
+.addField(': تاريخ دخولك لسيرفرنا', `\`${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')}  \` **\n ${moment(h.joinedAt).fromNow()} **`, true)
+.addField(` :لقد قمت بدعوة `, ` ${inviteCount} `)
+
+
+.setFooter(message.author.username, message.author.avatarURL)  
+message.channel.sendEmbed(id);
+})
+}
+
+
+
+});
+
 client.login(process.env.BOT_TOKEN);
