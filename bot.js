bd2cb93996edd58731c2b152790eafe3f23428c6
M3roof <43769947+M3roof@users.noreply.github.com>
2018-11-28 03:39:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -936,24 +936,29 @@ client.on('guildMemberAdd', member => {
 
 
 
-    client.on('message', message => {
-        var prefix = "$";
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-      
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-      
-      
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-        
-       
-      });
+var prefix = "$";
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+ 
+
+if (command == "say1") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
 
 
 
