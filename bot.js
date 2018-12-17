8a2472563f890d4cd656000bbea61629f542fcf0
M3roof <43769947+M3roof@users.noreply.github.com>
2018-12-17 03:50:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,31 +12,41 @@ client.on('ready', () => {
 
 
 
-
-client.on('message', message => {
+client.on("message", message => {
     var prefix = ".";
-    
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
         let args = message.content.split(' ').slice(1).join(' ');
     if(message.content.split(' ')[0] == prefix + 'bc') {
         if (!args[1]) {
-    message.channel.send("**يرجأء كتابة الرسالة المراد ارسالها للجميع**");
+    message.channel.send("**يرجاء كتابة الرسالة المراد ارسالها للجميع**");
     return;
     }
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                .addField('Message', args)
-                .setColor('#ff0000')
-                m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
+                m.send(args);
             });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✅| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📝| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
         }
         } else {
             return;
         }
     });
+
+
+
+
+
+
+
 
 
 
