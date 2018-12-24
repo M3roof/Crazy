579d689c04eafbac68735593545ecc1a3bf4603b
M3roof <43769947+M3roof@users.noreply.github.com>
2018-12-24 23:37:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ const ytdl = require('ytdl-core');
 const moment = require('moment');
 const request = require('request');
 const fs = require("fs");
-var adminprefix = '*'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -218,7 +217,7 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
 const developers = ["509243410234867724","id"]
 client.on('message', message => {
-
+var adminprefix = '*'
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
   if (message.content.startsWith(adminprefix + 'setname')) {
@@ -266,7 +265,50 @@ client.on('ready', function(){//npm i ms
 
 
 
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","✵-「p-chat");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:norelden});          
+               
+ 
+      }
+      });
 
+
+
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "✵-「p-chat");
+     stewart.send(`<@${member.user.id}> invited By  <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
 
 
 
