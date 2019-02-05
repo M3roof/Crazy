80963274cb58519c952b343916a4735f5db371e5
M3roof <43769947+M3roof@users.noreply.github.com>
2019-02-05 19:27:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,80 +31,37 @@ client.on('ready', () => {
 
 
 
-client.on("message", message => {
-    var prefix = "*";
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**يرجاء كتابة الرسالة المراد ارسالها للجميع**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                m.send(args);
-            });
-            const AziRo = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)   
-            .setTitle('✅| جاري ارسال رسالتك ') 
-            .addBlankField(true)
-            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-            .addField('📝| الرسالة ', args)
-            .setColor('RANDOM')  
-            message.channel.sendEmbed(AziRo);          
-        }
-        } else {
-            return;
-        }
-    });
+client.on('ready', async() => {
+var server = "516364082127831040"; // ايدي السررفر
+var channel = "541717117926506517";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
 
 
 
+client.on('ready', async() => {
+var server = "516364082127831040"; // ايدي السررفر
+var channel = "541717128131379204";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-client.on("message", message => {
-  if(message.content.startsWith("*verify")) {
-    let num = Math.floor((Math.random() * 4783) + 10);
-
-    message.channel.send(`يرجاء كتابة الرقم التالي: **${num}**`).then(m => {
-      message.channel.awaitMessages(res => res.content == `${num}`, {
-        max: 1,
-        time: 60000,
-        errors: ['time'],
-      }).then(collected => {
-        message.delete();
-        m.delete();
-        message.member.addRole(message.guild.roles.find(c => c.name == "• SnOw » Member"));
-      }).catch(() => {
-        m.edit(`You took to long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));
-      });
-    });
-  }
-});
-
-
-
-
-
-
+client.on('ready', async() => {
+var server = "516364082127831040"; // ايدي السررفر
+var channel = "541717138142920745";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
 
 
 
     client.on('message', message => {
-        var prefix = "*";
+        var prefix = "2";
         if (message.author.bot) return;
         if (!message.content.startsWith(prefix)) return;
       
@@ -121,330 +78,6 @@ client.on("message", message => {
         
        
       });
-
- 
-
-var prefix = "*";
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
- 
-
-if (command == "1say") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor(0x23b2d6)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-
-
-});
-
-
-
-
-
-
-
-client.on('message', message => {
- var prefix = "*"
-
-if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-    const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`\`Support Team\` **لا توجد رتبة بأسم**`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`**لديك تذكرة مفتوحة بالفعل**`);
-    message.guild.createChannel(`ticket`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support Team");
-        let role2 = message.guild.roles.find("name", "@everyone");
-        c.overwritePermissions(role, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        c.overwritePermissions(role2, {
-            SEND_MESSAGES: false,
-            READ_MESSAGES: false
-        });
-        c.overwritePermissions(message.author, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        message.channel.send(`:white_check_mark: تم انشاء التذكرة`);
-        const embed = new Discord.RichEmbed()
-        .setColor(0xCF40FA)
-        .addField(`${message.author.username} **مرحبا بك**`, `
-يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم ** ** هنا قريباً لمساعدتك`)
-        .setTimestamp();
-        c.send({ embed: embed });
-    }).catch(console.error);
-}
-if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج قناة التذاكر`);
-
-    message.channel.send(`**confirm** : هل انت متأكد من اغلاق التذكرة ؟ اذا انت متأكد اكتب`)
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === 'confirm', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('انتهي وقت اغلاق التذكرة').then(m2 => {
-              m2.delete();
-          }, 3000);
-        });
-    });
-}
-
-});
-
-
-
-
-
-
-
-
-const developers = ["509243410234867724","id"]
-client.on('message', message => {
-var adminprefix = '*'
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('ready', function(){//npm i ms 
-    var ms = 60000 ;
-    var setGame = [`SnOw`,'SnOw Code'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/m3roof`);
-    }, ms);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-var prefix = "*"
-    if (message.content.startsWith(prefix + 'clear')) {
-      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`ليس لديك برمشن[*MANAGE_MESSAGES*] `).catch(console.error);
-  message.delete()
-  if(!message.channel.guild) return;
-  let args = message.content.split(" ").slice(1);
-  
-  const messagecount = parseInt(args.join(' '));
-  
-  message.channel.fetchMessages({
-  
-  limit: messagecount
-  
-  }).then(messages => message.channel.bulkDelete(messages));
-  message.channel.sendMessage("", {embed: {
-    title: "``تم مسح الشات``",
-    color: 0x06DF00,
-    footer: {
-    
-    }
-    }}).then(msg => {msg.delete(3000)});
-  };
-  
-  });
-
-
-
-
-
-
-	
-client.on('message', async rokz => {
-var prefix = "*"
-
-    if(rokz.content.startsWith(prefix + "تقديم")) {
-
-      let lang = '';
-
-      let time = '';
-
-      let expe = '';
-
-      let fillter = m => m.author.id === rokz.author.id
-
-      await rokz.channel.send("**ما لغت البرمجه الخاصه بك ؟**").then(e => {
-
-     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-
-     .then(co => {
-
-       lang = co.first().content;
-
-        co.first().delete();
-
-
-       e.edit(`ما هوا الفرق بين var و const 
-[${lang}]`)
-
-       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-
-       .then(col => {
-
-         time = col.first().content;
-
-          col.first().delete();
-
-
-            e.edit(`مدة الخبرة لديك ؟
-[${time}]
-[${lang}]`)
-
-            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
-
-            .then(coll => {
-
-              expe = coll.first().content;
-
-               coll.first().delete();
-
-
-               e.edit(`جاري تقديمك...
-[${expe}]
-[${time}]
-[${lang}]`)
-
-              let rokzz = rokz.guild.channels.find("name","✵-「التقديمات")
-
-              setTimeout(() => {
-
-                e.edit("**تم التقديم**")
-
-              }, 3000)
-
-              rokzz.send(`
-» اللغة : **${lang}**
-» المدة : **${time}**
-» الخبرة : **${expe}**
-تم التقديم بواسطة: ${rokz.author}
-`).then(rokzzz => {
-
-                  rokzzz.react(":CheckMark:")
-
-                  rokzzz.react(":WrongMark:")
-
-                })
-
-            })
-
-       })
-
-     })
-
-   })
-
-    }
-
-  })
-  
-
-  
-
-client.on('message',async message => {
-var prefix = "*"
-
-let mention = message.mentions.members.first();
-
-let Room = client.channels.get('525276144329556039');
-
-if(message.content.startsWith(prefix + "قبول")) {
-
-if(message.guild.id !== '498226120215494667') return;
-
- if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
-
-
-if(!mention) return message.reply("منشن شخص");
-
-
-
-Room.send(`
-**» العضو :** ${mention}
-[ :white_check_mark: ] :: لقد تم قبول العضو واعطائه رتبة سبورت`);
-
-}
-
-});
-	
-	
-	  client.on('message',async message => {
-var prefix = "*"
-
-let mention = message.mentions.members.first();
-
-let Room = client.channels.get('525276144329556039');
-
-if(message.content.startsWith(prefix + "رفض")) {
-
-if(message.guild.id !== '498226120215494667') return;
-
- if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
-
-
-if(!mention) return message.reply("منشن شخص");
-
-
-
-Room.send(`
-**» العضو :** ${mention}
-[ :x: ] :: لقد تم رفض العضو`);
-
-}
-
-});
    
 
 
